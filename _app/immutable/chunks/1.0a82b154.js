ccf11ccfd230e6098ab47e8d52046aff383a24d7
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-03-29 20:49:04 +0000

    Deploying to gh-pages from @ Trust-Machines/sbtc-bridge-web@bf4af98c8efbba46afdd328bf5defdd92f5de14e 🚀

--- _app/immutable/chunks/1.0a82b154.js
+++
@@ -1,4 +0,0 @@
-import { default as default2 } from "../entry/error.svelte.ce7caa80.js";
-export {
-  default2 as component
-};
--- _app/immutable/chunks/navigation.28021d5b.js
+++ _app/immutable/chunks/navigation.28021d5b.js
@@ -1,4 +1,4 @@
-import { j as client } from "./singletons.5d4a9a90.js";
+import { j as client } from "./singletons.5c7fce7b.js";
 client.disable_scroll_handling;
 const goto = client.goto;
 client.invalidate;
