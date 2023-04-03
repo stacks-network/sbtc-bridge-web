095d872e47f312a17ad23d7cc1c18d03717f9913
igorsyl <igorsyl@users.noreply.github.com>
2023-04-03 01:00:57 +0000

    Deploying to gh-pages from @ Trust-Machines/sbtc-bridge-web@b91741cb748b0848a54e9d20ca43514a008f9c11 🚀

--- _app/immutable/chunks/1.fd4262b0.js
+++
@@ -1,4 +0,0 @@
-import { default as default2 } from "../entry/error.svelte.b9aba2cf.js";
-export {
-  default2 as component
-};
--- _app/immutable/chunks/navigation.39e2b376.js
+++ _app/immutable/chunks/navigation.39e2b376.js
@@ -1,4 +1,4 @@
-import { j as client } from "./singletons.34f53ff5.js";
+import { j as client } from "./singletons.ed34ca1f.js";
 client.disable_scroll_handling;
 const goto = client.goto;
 client.invalidate;
