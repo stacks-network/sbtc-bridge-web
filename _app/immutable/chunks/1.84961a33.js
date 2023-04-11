227ae4bd7a68ae581bc8e49d7f43c9ef67655978
igorsyl <igorsyl@users.noreply.github.com>
2023-04-11 16:53:08 +0000

    Deploying to gh-pages from @ Trust-Machines/sbtc-bridge-web@9a98d5a005851fca4338acda24fd19c333278144 🚀

---
+++ _app/immutable/chunks/1.84961a33.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../entry/error.svelte.f6e7c501.js";
+export {
+  default2 as component
+};
--- _app/immutable/chunks/2.895d9d00.js
+++ _app/immutable/chunks/2.895d9d00.js
@@ -1,5 +1,5 @@
 import { _ } from "./_page.061ae9e4.js";
-import { default as default2 } from "../entry/_page.svelte.77662dd6.js";
+import { default as default2 } from "../entry/_page.svelte.67adc987.js";
 export {
   default2 as component,
   _ as universal
