91a45a662f7453452e47358e82cf1a5500b6eeab
Mike Cohen <mjoecohen@gmail.com>
2023-04-12 20:27:43 +0100

    bug - copy fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -81,9 +81,9 @@ onMount(async () => {
 </div>
 {:else}
 <div class="lobby bg-dark">
-  <p class="text-white">Connect your Hiro web wallet to start wrapping sBTC!</p>
+  <p class="text-white">Connect to a Stacks Web Wallet to start Wrapping!</p>
   <p><span class="nav-item"><a href="/" class="pointer px-2" on:click|preventDefault={doLogin} ><span  class="px-1"><img src={stx_eco_wallet_off} alt="Connect Wallet / Login" width="40" height="auto"/></span> connect</a></span></p>
-  <p class="mt-5 text-warning">Currently in Alpha Testing!</p>
+  <p class="mt-5 text-warning">sBTC Alpha Testing!</p>
 </div>
 {/if}
 {/if}
