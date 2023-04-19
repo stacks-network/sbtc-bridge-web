cdfa89a9927f225c6e69f55e3d6f1bd25ef001ce
Igor Sylvester <1248500+igorsyl@users.noreply.github.com>
2023-04-19 10:56:30 -0500

    Fixing CORS policy (#99)
    * update
    
    * update
    
    * update

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# gsutil iam ch allUsers:objectViewer gs://sbtc-bridge-web
+
 DOCKER_BUILDKIT=1 docker build --file Dockerfile --progress=plain --output build .
 gcloud storage rm --recursive "gs://sbtc-bridge-web/**"
 gcloud storage cp --recursive build/* gs://sbtc-bridge-web/
