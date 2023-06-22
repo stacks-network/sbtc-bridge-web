7ce18a40fc747886c84875efeb9264a7b02cb4be
Maxime Laforet <me@macx.im>
2023-06-22 13:23:57 +0100

    fix: design implementation (#154)
    * fix: design implementation
    
    * fix: design implementation
    
    * fix: design implementation
    
    * fix: design implementation
    
    * fix: design implementation

--- src/lib/components/common/ServerError.svelte
+++ src/lib/components/common/ServerError.svelte
@@ -3,34 +3,35 @@ import { sbtcConfig } from '$stores/stores'
 import { CONFIG } from '$lib/config';
 import { goto } from "$app/navigation";
 import Button from '$lib/components/shared/Button.svelte';
+	import { a_primary } from '$lib/css_utils';
 
 const launchApp = () => {
-    goto('/?net=' + CONFIG.VITE_NETWORK)
+  goto('/?net=' + CONFIG.VITE_NETWORK)
 }
 </script>
-<div class="mx-auto h-[calc(100vh-280px)] flex flex-col justify-center">
-    <div class="mx-auto max-w-2xl">
-        <div class="flex flex-col p-10 gap-0 items-start bg-gray-1000 border-[0.5px] border-gray-700 rounded-3xl">
-            <div class="flex flex-col gap-0">
-                {#if !$sbtcConfig?.sbtcContractData?.sbtcWalletAddress}
-                <h1 class="font-medium text-2xl">Error: sBTC Wallet</h1>
-                {:else}
-                <h1 class="font-medium text-2xl">Server Error!</h1>
-                {/if}
-                {#if !$sbtcConfig?.sbtcContractData?.sbtcWalletAddress}
-                <p class="text-sm font-light">The sBTC wallet is undefined in the sBTC Contract.</p>
-                <p class="text-sm font-light">The Bridge is waiting for configuration data to be setup.</p>
-                {:else if CONFIG.VITE_NETWORK === 'mainnet'}
-                <p class="text-sm font-light">Mainnet is not yet fully supported.</p>
-                <p class="text-sm font-light">Please use the settings menu to switch to testnet.</p>
-                {:else}
-                <p class="text-sm font-light">Temporarily problems connecting to the bridge.</p>
-                <p class="text-sm font-light">Please try later.</p>
-                {/if}
-                <p class="text-sm font-light">For more info about the sBTC Bridge, check <a class="text-hint-link" href="/faq">the FAQ page.</a></p>
-                <Button darkScheme={false} label={'Back home'} target={'launchApp'} on:clicked={() => launchApp()}/>
-            </div>
-        </div>
+
+<div class="mx-auto flex flex-col justify-center px-6 lg:px-8 py-6">
+  <div class="max-w-2xl">
+    <div class="flex flex-col p-10 gap-6 items-start bg-gray-1000 border-[0.5px] border-gray-700 rounded-3xl">
+      {#if !$sbtcConfig?.sbtcContractData?.sbtcWalletAddress}
+        <h1 class="text-4xl font-normal">Error: sBTC Wallet</h1>
+      {:else}
+        <h1 class="text-4xl font-normal">Server Error!</h1>
+      {/if}
+      <div>
+        {#if !$sbtcConfig?.sbtcContractData?.sbtcWalletAddress}
+          <p class="mb-2">The sBTC wallet is undefined in the sBTC Contract.</p>
+          <p class="mb-2">The Bridge is waiting for configuration data to be setup.</p>
+        {:else if CONFIG.VITE_NETWORK === 'mainnet'}
+          <p class="mb-2">Mainnet is not yet fully supported.</p>
+          <p class="mb-2">Please use the settings menu to switch to testnet.</p>
+        {:else}
+          <p class="mb-2">Temporarily problems connecting to the bridge.</p>
+          <p class="mb-2">Please try later.</p>
+        {/if}
+        <p class="mb-2">For more info about the sBTC Bridge, check <a class={a_primary} href="/faq">the FAQ page.</a></p>
+      </div>
+      <Button darkScheme={false} label={'Back home'} target={'launchApp'} on:clicked={() => launchApp()}/>
     </div>
+  </div>
 </div>
-      
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,36 +3,38 @@
   import { a_primary } from '$lib/css_utils';
 </script>
 
-<div class="mx-auto h-[calc(100vh-280px)] flex flex-col justify-center">
-  <div class="mx-auto max-w-2xl">
+<div class="mx-auto flex flex-col justify-center px-6 lg:px-8 py-6">
+  <div class="max-w-2xl">
     <div class="flex flex-col p-10 gap-6 items-start bg-gray-1000 border-[0.5px] border-gray-700 rounded-3xl">
-      <div class="flex flex-col">
-        <h1 class="text-4xl font-normal">Welcome!</h1>
-        <div class="flex flex-col gap-1">
-          <p>
-            The <span class="font-normal">sBTC Bridge</span> is a community led Stacks project. The Bridge 
-            provides a non-custodial, permissionless way to move Bitcoin into and out of the Stacks Blockchain.
-          </p>
-          <p>
-            The Bridge provides features to build and track Bitcoin and Stacks transactions: it doesn't take 
-            custody of your money.
-          </p>
-          <p>
-            In order to interact with this application, you will need to have a wallet that supports both BTC and STX. <a class={a_primary} href="/wallet">Don't have a wallet?</a>
-          </p>
-          <p>
-            The Bridge services are free - we do not add any fees. You will however need to allow for payment of 
-            Bitcoin gas fees. We hope to add support for the Lightning Network in a future release to help
-            reduce the overheads even further.
-          </p>
-          <p class="">For more info about the sBTC Brigde, check <a class={a_primary} href="/faq">the FAQ page.</a></p>
-        </div>
-        <div>
-          <a href="/start" class="inline-flex items-center gap-x-1.5 bg-primary-01 px-4 py-2 font-normal text-black rounded-xl border border-primary-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50" role="button">
-            Launch app
-            <Icon src="{ArrowSmallRight}" mini class="-mr-0.5 h-5 w-5" aria-hidden="true" />
-          </a>
-        </div>
+      <h1 class="text-4xl font-normal">Welcome!</h1>
+      <div>
+        <p class="mb-2">
+          The <span class="font-normal">sBTC Bridge</span> is a community led Stacks project. The Bridge
+          provides a non-custodial, permissionless way to move Bitcoin into and out of the Stacks Blockchain.
+        </p>
+        <p class="mb-2">
+          The Bridge provides features to build and track Bitcoin and Stacks transactions: it doesn't take
+          custody of your money.
+        </p>
+        <p class="mb-2">
+          In order to interact with this application, you will need to have a wallet that supports both BTC and STX. <a class={a_primary} href="/wallet">Don't have a wallet?</a>
+        </p>
+        <p class="mb-2">
+          The Bridge services are free - we do not add any fees. You will however need to allow for payment of
+          Bitcoin gas fees. We hope to add support for the Lightning Network in a future release to help
+          reduce the overheads even further.
+        </p>
+        <p class="">For more info about the sBTC Brigde, check <a class={a_primary} href="/faq">the FAQ page.</a></p>
+      </div>
+      <div class="flex items-center gap-2">
+        <a href="/start" class="inline-flex items-center gap-x-1.5 bg-primary-01 px-4 py-2 font-normal text-black rounded-xl border border-primary-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50" role="button">
+          Launch app
+          <Icon src="{ArrowSmallRight}" mini class="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </a>
+
+        <a href="/how-it-works" class="inline-flex items-center gap-x-1.5 bg-transparent px-4 py-2 font-normal text-white rounded-xl border border-transparent hover:bg-white/[8%] focus:bg-white/[16%] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50" role="button">
+          Learn more
+        </a>
       </div>
     </div>
   </div>
