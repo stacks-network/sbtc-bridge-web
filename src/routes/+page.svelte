c25d43ba1db218181e42bdc669b47bc9120c4e78
Maxime Laforet <me@macx.im>
2023-06-22 12:58:24 +0100

    fix: design implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
   <div class="max-w-2xl">
     <div class="flex flex-col p-10 gap-6 items-start bg-gray-1000 border-[0.5px] border-gray-700 rounded-3xl">
       <h1 class="text-4xl font-normal">Welcome!</h1>
-      <div class="flex flex-col gap-1">
+      <div>
         <p class="mb-2">
           The <span class="font-normal">sBTC Bridge</span> is a community led Stacks project. The Bridge
           provides a non-custodial, permissionless way to move Bitcoin into and out of the Stacks Blockchain.
@@ -26,10 +26,14 @@
         </p>
         <p class="">For more info about the sBTC Brigde, check <a class={a_primary} href="/faq">the FAQ page.</a></p>
       </div>
-      <div>
+      <div class="flex items-center gap-2">
         <a href="/start" class="inline-flex items-center gap-x-1.5 bg-primary-01 px-4 py-2 font-normal text-black rounded-xl border border-primary-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50" role="button">
           Launch app
           <Icon src="{ArrowSmallRight}" mini class="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </a>
+
+        <a href="/how-it-works" class="inline-flex items-center gap-x-1.5 bg-transparent px-4 py-2 font-normal text-white rounded-xl border border-transparent hover:bg-white/[8%] focus:bg-white/[16%] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50" role="button">
+          Learn more
         </a>
       </div>
     </div>
