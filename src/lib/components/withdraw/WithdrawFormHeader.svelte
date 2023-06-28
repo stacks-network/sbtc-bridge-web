4186a78cc653b2d233b80bb2c6014924c7e6338f
Maxime Laforet <me@macx.im>
2023-06-28 23:48:59 +0100

    fix: design implementation

--- src/lib/components/withdraw/WithdrawFormHeader.svelte
+++ src/lib/components/withdraw/WithdrawFormHeader.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-import { onMount } from 'svelte';
-import SwitchGraphic from '$lib/components/deposit/SwitchGraphic.svelte';
+  import { onMount } from 'svelte';
+  import SwitchGraphic from '$lib/components/deposit/SwitchGraphic.svelte';
 
-onMount(async () => {
-})
-
+  onMount(async () => {
+  })
 </script>
 
-<div class="flex w-full mt-5 gap-y-4 align-baseline items-center flex-wrap">
-  <div class="grow">
-    <span class="text-2xl font-medium">Withdraw BTC</span>
-  </div>
-  <div class="grow ">
-    <SwitchGraphic reverse={true}/>
+<div class="flex w-full items-center justify-between flex-wrap mb-10">
+  <h2 class="text-4xl font-medium">
+    Withdraw BTC
+  </h2>
+  <div class="-mr-10">
+    <SwitchGraphic reverse={false}/>
   </div>
 </div>
