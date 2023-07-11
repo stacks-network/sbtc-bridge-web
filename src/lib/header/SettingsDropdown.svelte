8eb99ba85865494a8a0f140c5e4181f0594bb736
Mike Cohen <mjoecohen@gmail.com>
2023-07-11 13:31:06 +0100

    Bug: fix for incorrect btnClass attribute on setting/account menu buttons.

--- src/lib/header/SettingsDropdown.svelte
+++ src/lib/header/SettingsDropdown.svelte
@@ -71,7 +71,7 @@
 	}
 </script>
 
-<Button btnClass="block w-full md:inline-flex bg-primary-02 p-px font-normal rounded-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50">
+<Button class="block w-full md:inline-flex bg-primary-02 p-px font-normal rounded-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500/50">
 	<span class="block w-full md:inline-flex items-center gap-x-1.5 bg-gray-1000 px-4 py-2 rounded-xl h-full">
 		<span class="text-transparent bg-clip-text bg-primary-02">Settings</span>
 
