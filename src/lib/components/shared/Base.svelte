c8f35381c16050ae833bcc4b337a3ce24e7365c4
Maxime Laforet <me@macx.im>
2023-09-18 23:16:43 +0100

    feat: create Base component to DRY stuff a bit

---
+++ src/lib/components/shared/Base.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Dashboard from '$lib/components/dashboard/Dashboard.svelte';
+	import Flow from '$lib/components/dashboard/Flow.svelte';
+  export let title: string;
+</script>
+
+
+<svelte:head>
+  <title>sBTC Bridge - {title}</title>
+  <meta name="description" content="The sBTC Bridge
+  provides a non-custodial, permissionless way to move Bitcoin into and out of the Stacks Blockchain." />
+</svelte:head>
+
+<div class="mx-auto max-w-7xl px-6 py-6">
+  <div class="my-8">
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-6">
+      <Dashboard />
+      <Flow />
+    </div>
+  </div>
+</div>
