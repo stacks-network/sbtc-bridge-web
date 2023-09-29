48acf78611a77ced440052b6a4ab493e9caf5a15
Maxime Laforet <me@macx.im>
2023-09-29 14:39:02 +0100

    feat: Add DashboardBase component to prevent duplication (#242)
    * feat: create Base component to DRY stuff a bit
    
    * chore: rename component

--- src/routes/deposit/+page.svelte
+++ src/routes/deposit/+page.svelte
@@ -1,21 +1,6 @@
 <script lang="ts">
-	import Dashboard from '$lib/components/dashboard/Dashboard.svelte';
-	import Flow from '$lib/components/dashboard/Flow.svelte';
-
+	import DashboardBase from '$lib/components/shared/DashboardBase.svelte';
 </script>
 
 
-<svelte:head>
-  <title>sBTC Bridge - Dashboard</title>
-  <meta name="description" content="The sBTC Bridge
-  provides a non-custodial, permissionless way to move Bitcoin into and out of the Stacks Blockchain." />
-</svelte:head>
-
-<div class="mx-auto max-w-7xl px-6 py-6">
-  <div class="my-8">
-    <div class="grid grid-cols-1 gap-8 lg:grid-cols-6">
-      <Dashboard />
-      <Flow />
-    </div>
-  </div>
-</div>
+<DashboardBase title="Deposit BTC, mint sBTC" />
