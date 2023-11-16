195190351a396648a90fec48e40c02212b7d84fb
Mike Cohen <mjoecohen@gmail.com>
2023-11-16 23:01:50 +0000

    sorting out the config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,15 +1,15 @@
-const DEVENV_CONFIG = {
+const SHARED_DEVENV_CONFIG = {
     VITE_ENVIRONMENT: 'devenv',
     VITE_PUBLIC_APP_NAME: 'sBTC Bridge Devenv',
     VITE_PUBLIC_APP_VERSION: '1.0.0',
     VITE_NETWORK: 'devnet',
     VITE_SBTC_COORDINATOR: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
     VITE_BRIDGE_WS: 'ws://localhost:3010',
-    VITE_BRIDGE_API: 'http://localhost:3010/bridge-api/v1',
-    VITE_STACKS_API: 'http://96.126.107.204:3999',
-    VITE_STACKS_EXPLORER: 'http://96.126.107.204:3020',
-    VITE_BSTREAM_EXPLORER: 'http://96.126.107.204:8083/api',
-    VITE_MEMPOOL_EXPLORER: 'http://96.126.107.204:8083',
+    VITE_BRIDGE_API: 'http://45.79.130.153:3010/bridge-api/v1',
+    VITE_STACKS_API: 'http://45.79.130.153:3999',
+    VITE_STACKS_EXPLORER: 'http://45.79.130.153:3020',
+    VITE_BSTREAM_EXPLORER: 'http://45.79.130.153:8083/api',
+    VITE_MEMPOOL_EXPLORER: 'http://45.79.130.153:8083',
 }
 
 const DEVNET_CONFIG = {
@@ -24,20 +24,6 @@ const DEVNET_CONFIG = {
     VITE_STACKS_EXPLORER: 'http://stacks-explorer:3020',
     VITE_BSTREAM_EXPLORER: 'http://mempool-web/testnet/api',
     VITE_MEMPOOL_EXPLORER: 'http://mempool-web/testnet/api',
-}
-
-const DEV_TESTNET_CONFIG = {
-    VITE_ENVIRONMENT: 'testnet',
-    VITE_PUBLIC_APP_NAME: 'sBTC Bridge Devnet',
-    VITE_PUBLIC_APP_VERSION: '1.0.0',
-    VITE_NETWORK: 'testnet',
-    VITE_SBTC_COORDINATOR: 'ST1R1061ZT6KPJXQ7PAXPFB6ZAZ6ZWW28G8HXK9G5',
-    VITE_BRIDGE_WS: 'ws://localhost:3010',
-    VITE_BRIDGE_API: 'https://bridge.sbtc.tech/bridge-api/v1',
-    VITE_STACKS_API: 'https://api.testnet.hiro.so',
-    VITE_STACKS_EXPLORER: 'https://explorer.hiro.so',
-    VITE_BSTREAM_EXPLORER: 'https://mempool.space/testnet',
-    VITE_MEMPOOL_EXPLORER: 'https://mempool.space/testnet/api',
 }
 
 const TESTNET_CONFIG = {
@@ -71,33 +57,16 @@ const MAINNET_CONFIG = {
 export let CONFIG = MAINNET_CONFIG;
 
 export function setConfig(network:string) {
-    if (!network || network.indexOf('chain=') === -1) network = 'testnet'
-    else if (network.indexOf('devenv') > -1) {
-        CONFIG = DEVENV_CONFIG;
-        return
-    }
-    else if (network.indexOf('devnet') > -1) {
-        CONFIG = DEVNET_CONFIG;
-        return
-    }
-    else if (network.indexOf('devnet') === -1) network = 'devnet'
-    else if (network.indexOf('testnet') === -1) network = 'testnet'
-    else if (network.indexOf('mainnet') === -1) network = 'mainnet'
     const mode = import.meta.env.MODE
-    if (mode === 'development') {
-        if (network === 'testnet' || network === 'mainnet') CONFIG = DEV_TESTNET_CONFIG;
-        else CONFIG = DEVNET_CONFIG;
+    if (mode === 'shared-devenv') {
+        CONFIG = SHARED_DEVENV_CONFIG;
         return;
-    } else if (mode === 'devenv') {
+    } else if (mode === 'devenv' || mode === 'development' || mode === 'dev') {
         CONFIG = DEVNET_CONFIG;
         return;
     }
-    //if (!mode) mode = 'testnet'
-    //console.log('import.meta.env.MODE: ' + mode);
-	if (network === 'devnet') CONFIG = DEVNET_CONFIG;
-	else if (network.indexOf('testnet') > -1) CONFIG = TESTNET_CONFIG;
+
+    if (!network || network.indexOf('chain=') === -1) network = 'testnet'
+    else if (network.indexOf('testnet') > -1) CONFIG = TESTNET_CONFIG;
 	else CONFIG = MAINNET_CONFIG
-    if (import.meta.env.MODE === 'linode-staging') {
-        CONFIG.VITE_BRIDGE_API = 'https://bridge.sbtc.tech/bridge-api/v1'
-    }
 }
